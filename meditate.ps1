f7ff257a5a03556d83f1f5f102ca9764ef1e5541
Miles Dowe <milesdowe@gmail.com>
2020-01-24 14:53:33 -0800

    Sync scripts with personal

---
+++ meditate.ps1
@@ -0,0 +1,4 @@
+# For when I try to meditate.
+# I like to drown out sound with the given youtube video for 30 minutes.
+
+& 'C:/Program Files/Mozilla Firefox/firefox.exe' 'https://www.youtube.com/watch?v=Qd3N_r0VUWQ' 'https://duckduckgo.com/?q=timer+for+30+minutes&ia=answer'
